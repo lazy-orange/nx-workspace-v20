b256e22023141bd35375042db089d15198a5a22c
Vitalii Markov <vymarkov@gmail.com>
2024-12-11 23:32:45 +0000

    feat: import utils using "exports" field

--- apps/buz/src/main.ts
+++ apps/buz/src/main.ts
@@ -3,17 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
+import { now } from "@lazyorange/foo/utils";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = "api";
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    {
+      time: now(),
+    }
+  );
 }
 
 bootstrap();
