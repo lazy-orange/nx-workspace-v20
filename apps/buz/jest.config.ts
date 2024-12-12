e45bd28a15dc0556ea0c72be616612002ad55286
Vitalii Markov <vymarkov@gmail.com>
2024-12-12 18:42:30 +0000

    chore: unable to build app with lib with exports fields

--- apps/buz/jest.config.ts
+++ apps/buz/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'buz',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
+  displayName: "buz",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/buz'
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/apps/buz",
 };
